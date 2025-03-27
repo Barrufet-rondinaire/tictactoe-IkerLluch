aa7e1dec542362529e26712cae9b0b6674be43cf
iker <illuch@cendrassos.net>
2025-03-27 19:52:29 +0100

    DIa 3 o 2

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace TicTacToe;
-
+﻿using System.Net.Http.Json;
+namespace TicTacToe;
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+        using (var client = new HttpClient())
+        {
+            var r = await client.GetFromJsonAsync <List<string>> (
+                      "http://localhost:8080/jugadors"
+            );
+            foreach (var comentari in r)
+            {
+                
+            }
+            {
+                
+            }
+
+        }
+       
+
+
     }
 }
+
+
