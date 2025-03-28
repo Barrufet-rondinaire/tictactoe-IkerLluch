644e4c15a3ff54e1f50272521b36376cd98d6aab
iker <illuch@cendrassos.net>
2025-03-28 17:40:22 +0100

    Dia4

---
+++ TicTacToe/Partida.cs
@@ -0,0 +1,7 @@
+public class Partida{
+    public int numero {get; set;}
+    public string nomJugador1 {get; set;}
+    public string nomJugador2 {get; set;}
+
+    public List<string> Tauler {get; set;}
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,4 +1,9 @@
-﻿using System.Net.Http.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 namespace TicTacToe;
 class Program
 {
@@ -6,16 +11,44 @@ class Program
     {
         using (var client = new HttpClient())
         {
-            var r = await client.GetFromJsonAsync <List<string>> (
+            var comentaris = await client.GetFromJsonAsync <List<string>> (
                       "http://localhost:8080/jugadors"
             );
-            foreach (var comentari in r)
+            
+            Dictionary<string, string> jugadors = new Dictionary<string, string>();
+            string patternPais = @"representa\w*\s\w*\s(?<country>[\w-]*)";
+            string patternPersona = @"participant\w*\s(?<Persona>\w*\s\w*)";
+            
+            foreach (var comentari in comentaris)
             {
-                
+                Match matchPersona = Regex.Match(comentari, patternPersona);
+                Match matchPais = Regex.Match(comentari, patternPais);
+
+                if (matchPersona.Success && matchPais.Success)
+                {
+                    string persona = matchPersona.Groups["Persona"].Value;
+                    string pais = matchPais.Groups["country"].Value;
+                    jugadors.Add(persona, pais);
+                }
             }
+            foreach (var jugador in jugadors)
             {
-                
+                Console.WriteLine($"Nom: {jugador.Key}, País: {jugador.Value}");
             }
+        
+
+            for (int i = 1; i <= 10000; i++ )
+            {
+                using (var partida = new HttpClient())
+                {
+                    var partidas = await client.GetFromJsonAsync <Partida>(
+                      $"http://localhost:8080/partida/{i}"
+                    );
+                }
+
+            }
+             
+
 
         }
        
